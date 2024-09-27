bc49a4ef46f2e014d6128ce9fc2f853ec09fe50f
ealpago <alpagoemre@icloud.com>
2024-09-27 13:23:22 +0300

    ViewControllers and Cells files ready

---
+++ GithubAPI/GithubAPI/Views/RepoOwnerTableViewCell/RepoOwnerTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  RepoOwnerTableViewCell.swift
+//  GithubAPI
+//
+//  Created by Emre Alpago on 26.09.2024.
+//
+
+import UIKit
+
+class RepoOwnerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
---
+++ GithubAPI/GithubAPI/Views/SearchHistoryTableViewCell/SearchHistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchHistoryTableViewCell.swift
+//  GithubAPI
+//
+//  Created by Emre Alpago on 27.09.2024.
+//
+
+import UIKit
+
+class SearchHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
