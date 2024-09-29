8fd3d1715db0e6ff28a4571928f8941c58bca0c3
ealpago <alpagoemre@icloud.com>
2024-09-29 14:30:09 +0300

    ListingStoryboard UI changed, viewModel created

---
+++ GithubAPI/GithubAPI/Modules/SearchDetail/ListingViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ListingViewModel.swift
+//  GithubAPI
+//
+//  Created by Emre Alpago on 29.09.2024.
+//
+
+import Foundation
+
+protocol ListingViewModelInterface {}
+
+final class ListingViewModel {}
+
+extension ListingViewModel: ListingViewModelInterface {}
