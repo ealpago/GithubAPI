76aae1fc0b01daccf5a5d667f3bb0d2c9078b3e8
ealpago <alpagoemre@icloud.com>
2024-09-29 14:13:22 +0300

    Helpers added

---
+++ GithubAPI/GithubAPI/Helpers/AlertPresentable.swift
@@ -0,0 +1,22 @@
+//
+//  AlertPresentable.swift
+//  GithubAPI
+//
+//  Created by Emre Alpago on 28.09.2024.
+//
+
+import UIKit
+
+protocol AlertPresentable {
+    func showError(title: String, message: String, buttonTitle: String, completion: @escaping()->())
+}
+
+extension AlertPresentable where Self: UIViewController {
+    func showError(title: String, message: String, buttonTitle: String, completion: @escaping()->()) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: buttonTitle, style: .cancel, handler: { action in
+            completion()
+        }))
+        self.present(alert, animated: true)
+    }
+}
