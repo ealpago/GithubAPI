60d5a52706ab3fc18a809e2e9819b412ee8f39d4
ealpago <alpagoemre@icloud.com>
2024-09-29 18:32:17 +0300

    ListingVC tableView deleted collectionView added. ListingVC base ready

--- GithubAPI/GithubAPI/Modules/SearchDetail/ListingViewController.swift
+++ GithubAPI/GithubAPI/Modules/SearchDetail/ListingViewController.swift
@@ -7,9 +7,91 @@
 
 import UIKit
 
+protocol ListingViewInterface: AnyObject, AlertPresentable {
+    var columns: Int { get }
+    var userName: String { get }
+
+    func prepareTableView()
+    func reloadData()
+    func willDisplay()
+    func showAlert()
+}
+
+struct ListingViewArguments {
+    var userName: String?
+}
+
 class ListingViewController: UIViewController {
-    
+    @IBOutlet private weak var buttonsStackView: UIStackView!
+    @IBOutlet private weak var changeUIButton: UIButton!
+    @IBOutlet private weak var sortByStarButton: UIButton!
+    @IBOutlet private weak var sortByCreatedDateButton: UIButton!
+    @IBOutlet private weak var sortByUpdatedDateButton: UIButton!
+    @IBOutlet private weak var reposCollectionView: UICollectionView!
+
+    private lazy var viewModel = ListingViewModel()
+    var arguments: ListingViewArguments?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.view = self
+        viewModel.viewDidLoad()
+    }
+
+    @IBAction private func changeUIButtonTapped(_ sender: UIButton) {}
+    @IBAction private func sortByStarButtonTapped(_ sender: UIButton) {}
+    @IBAction private func sortByCreatedDateButtonTapped(_ sender: UIButton) {}
+    @IBAction private func sortByUpdatedDateButtonTapped(_ sender: UIButton) {}
+}
+
+extension ListingViewController: ListingViewInterface {
+    var userName: String {
+        arguments?.userName ?? ""
+    }
+    
+    var columns: Int {
+        0
+    }
+    
+    func showAlert() {
+        showError(title: "", message: "", buttonTitle: "") {}
+    }
+
+    func prepareTableView() {
+        reposCollectionView.dataSource = self
+        reposCollectionView.delegate = self
+        reposCollectionView.register(RepoCollectionViewCell.nib, forCellWithReuseIdentifier: RepoCollectionViewCell.identifier)
+        reposCollectionView.reloadData()
+    }
+    
+    func reloadData() {
+        self.reposCollectionView.reloadData()
+    }
+
+    func willDisplay() {}
+}
+
+extension ListingViewController: UICollectionViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        viewModel.beginPagination(user: "", page: 0)
+    }
+}
+
+extension ListingViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.numberOfRowsInSection
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = reposCollectionView.dequeueReusableCell(withReuseIdentifier: RepoCollectionViewCell.identifier, for: indexPath) as? RepoCollectionViewCell else { return UICollectionViewCell() }
+        return cell
+    }
+}
+
+extension ListingViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.bounds.width
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        return viewModel.collectionViewLayout(width: collectionView.bounds.width, minimumSpacing: flowLayout.minimumInteritemSpacing, columns: columns)
     }
 }
--- GithubAPI/GithubAPI/Modules/SearchDetail/ListingViewModel.swift
+++ GithubAPI/GithubAPI/Modules/SearchDetail/ListingViewModel.swift
@@ -7,8 +7,60 @@
 
 import Foundation
 
-protocol ListingViewModelInterface {}
+protocol ListingViewModelInterface {
+    var view: ListingViewInterface? { get set }
+    var numberOfRowsInSection: Int { get }
 
-final class ListingViewModel {}
+    func viewDidLoad()
+    func beginPagination(user: String, page: Int)
+    func changeUI()
+    func sortByStar()
+    func sortByCreatedDate()
+    func sortByUpdatedDate()
+    func collectionViewLayout(width: CGFloat, minimumSpacing: CGFloat, columns: Int) -> CGSize
+}
 
-extension ListingViewModel: ListingViewModelInterface {}
+final class ListingViewModel {
+    weak var view: ListingViewInterface?
+    private var repos: [GitHubRepo] = []
+    private var sortedRepos: [GitHubRepo] = []
+
+    private func getRepos(user: String, page: Int) {
+        ReposStoreManager.shared.fetchRepos(user: user, page: page) { [weak self] repos in
+            guard let self = self else {return}
+            self.repos = repos
+            self.view?.reloadData()
+        }
+    }
+}
+
+extension ListingViewModel: ListingViewModelInterface {
+    func collectionViewLayout(width: CGFloat, minimumSpacing: CGFloat, columns: Int) -> CGSize {
+        let spaceBetweenCells = minimumSpacing * (CGFloat(columns) - 1)
+        let adjustedWidth = width - spaceBetweenCells
+        let width: CGFloat = adjustedWidth / CGFloat(columns)
+        let height: CGFloat = width * 2
+        return CGSize(width: width, height: height)
+    }
+    
+    func changeUI() {}
+    
+    var numberOfRowsInSection: Int {
+        sortedRepos.count
+    }
+
+    func viewDidLoad() {
+        self.view?.prepareTableView()
+        self.getRepos(user: view?.userName ?? "", page: 1)
+    }
+
+    func sortByStar() {}
+    
+    func sortByCreatedDate() {}
+    
+    func sortByUpdatedDate() {}
+
+    func beginPagination(user: String, page: Int) {
+        self.getRepos(user: view?.userName ?? "", page: 1)
+    }
+}
