97788c9bb1376bbb64da7046e41e33eb2c9fe6dd
ealpago <alpagoemre@icloud.com>
2024-09-30 00:43:20 +0300

    StoDate string extension added

--- GithubAPI/GithubAPI/Extensions/String+Extension.swift
+++ GithubAPI/GithubAPI/Extensions/String+Extension.swift
@@ -11,12 +11,19 @@ extension String {
     public var storyBoard: UIStoryboard {
         UIStoryboard(name: self, bundle: nil)
     }
-
+    
     public func viewController(identifier: String) -> UIViewController {
         storyBoard.instantiateViewController(withIdentifier: identifier)
     }
-
+    
     public var initialViewController: UIViewController {
         storyBoard.instantiateInitialViewController()!
+    }
+    
+    func toDate() -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ" // The date format
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0) // UTC time zone
+        return dateFormatter.date(from: self)
     }
 }
