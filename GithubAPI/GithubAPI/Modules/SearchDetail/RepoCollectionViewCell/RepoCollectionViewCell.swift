50c1b5d371b9bed91eba86f0ca5314c296bfbe71
ealpago <alpagoemre@icloud.com>
2024-09-29 18:31:19 +0300

    RepoTableViewCell deleted, CollectionViewCell added

---
+++ GithubAPI/GithubAPI/Modules/SearchDetail/RepoCollectionViewCell/RepoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RepoCollectionViewCell.swift
+//  GithubAPI
+//
+//  Created by Emre Alpago on 29.09.2024.
+//
+
+import UIKit
+
+class RepoCollectionViewCell: UICollectionViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
