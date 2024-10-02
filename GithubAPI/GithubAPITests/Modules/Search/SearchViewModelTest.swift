aa4090a2ebf8e8c99f14ebe5798ce5716121a98d
ealpago <alpagoemre@icloud.com>
2024-10-02 17:04:32 +0300

    SearchViewModelTest created

---
+++ GithubAPI/GithubAPITests/Modules/Search/SearchViewModelTest.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewModelTest.swift
+//  GithubAPITests
+//
+//  Created by Emre Alpago on 2.10.2024.
+//
+
+import XCTest
+@testable import GithubAPI
+
+final class SearchViewModelTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
