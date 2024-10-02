683fb92faa70cb26aebf73a79c23cd5719aa8f2e
ealpago <alpagoemre@icloud.com>
2024-10-03 01:00:29 +0300

    MockNetworkManager fixxed

--- GithubAPI/GithubAPITests/Managers/MockNetworkManager.swift
+++ GithubAPI/GithubAPITests/Managers/MockNetworkManager.swift
@@ -13,15 +13,15 @@ final class MockNetworkManager: NetworkManagerInterface {
     var invokedRequestCount = 0
     var invokedRequestParameters: (requestRoute: NetworkRouter, responseModel: Any)?
     var invokedRequestParametersList = [(requestRoute: NetworkRouter, responseModel: Any)]()
-    var stubbedRequestCompletionResult: (Result<T, NetworkError>, Void)?
+    var stubbedRequestCompletionResult: Any?
 
     func request<T: Codable>(requestRoute: NetworkRouter, responseModel: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void) {
         invokedRequest = true
         invokedRequestCount += 1
         invokedRequestParameters = (requestRoute, responseModel)
         invokedRequestParametersList.append((requestRoute, responseModel))
-        if let result = stubbedRequestCompletionResult {
-            completion(result.0)
+        if let result = stubbedRequestCompletionResult as? Result<T, NetworkError> {
+            completion(result)
         }
     }
 }
